d719c46a9b1c749af2741882f8e73c8307c5854b
ludwig-n <nick.ludwig.g@gmail.com>
2025-09-08 19:20:39 +0400

    Fix run_infer.py failing if certain columns are missing

--- evaluation/benchmarks/swe_bench/run_infer.py
+++ evaluation/benchmarks/swe_bench/run_infer.py
@@ -774,6 +774,15 @@ if __name__ == '__main__':
     logger.info(
         f'Loaded dataset {args.dataset} with split {args.split}: {len(swe_bench_tests)} tasks'
     )
+
+    # Insert dummy columns that OpenHands expects to be present
+    if "PASS_TO_PASS" not in swe_bench_tests:
+        swe_bench_tests["PASS_TO_PASS"] = "[]"
+    if "FAIL_TO_PASS" not in swe_bench_tests:
+        swe_bench_tests["FAIL_TO_PASS"] = "[]"
+    if "version" not in swe_bench_tests:
+        swe_bench_tests["version"] = "1.0"
+
     if DATASET_TYPE == 'SWE-Gym':
         with open(
             os.path.join(
